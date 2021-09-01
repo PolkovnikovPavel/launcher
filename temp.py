d7d595e542843e7c09f10c3538bcfb4f43363f11
pavel <pavelpolkovnikov334@gmail.com>
2021-09-01 20:39:32 +0700

    create easy installer

--- temp.py
+++ temp.py
@@ -1,8 +1,22 @@
 import requests
 
+options = requests.get('https://raw.githubusercontent.com/PolkovnikovPavel/launcher/master/options').text
 
-session = requests.Session()
-r = session.get('https://github.com/PolkovnikovPavel/collection_lfk/raw/master/old_version/%D0%B6%D1%83%D1%80%D0%BD%D0%B0%D0%BB%20%D0%BB%D1%84%D0%BA%20v-1.7.exe')
-print(r.content)
+with open('version.txt', encoding='utf-8') as f:
+    version, name = f.read().split(':')
+new_version = options.split('\n')[0]
 
+if version != new_version:
+    for data in options.split('\n')[1::]:
+        v, url = data.split(' - ')
+        if v == new_version:
+            r = requests.get(url)
+            with open(name, 'wb') as f:
+                f.write(r.content)
+            with open('version.txt', 'w', encoding='utf-8') as f:
+                f.write(f'{new_version}:{name}')
+            print('Обновление прошло успешно')
+            break
+elif version == new_version:
+    print('Уже установлена последняя версия')
 
